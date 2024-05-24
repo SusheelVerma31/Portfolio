90884653462bee0dcab2277aca69d83b8362487c
SusheelVerma31 <150130897+SusheelVerma31@users.noreply.github.com>
2024-05-24 10:19:35 +0530

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,521 @@
+import gender from "../data/images/age_gender.jpg"
+import Emotion from "../data/images/emotion.jpg"
+import SVM from "../data/images/SVM.jpg"
+import Parking_Space from "../data/images/parking_space counter.jpg"
+import Number_Plate_Detection from "../data/images/number_plate.jpg"
+import Color_Detection from "../data/images/color.jpg"
+import Movie_Recommended_System from "../data/images/movie.jpg"
+import SMS_Spam_Detection from "../data/images/spam.jpg"
+import Olympic_Data_Analysis from "../data/images/olympic.jpg"
+import WhatsApp_Chat_Analysis from "../data/images/whatsapp.jpg"
+import Twitter_Sentiment_Analysis from "../data/images/twitter.png"
+import Resume_Screening_App from "../data/images/resume.jpg"
+import pratap from "../data/images/pratap.jpg"
+import google from "../data/images/google.png"
+import nlp from "../data/images/NlP.jpg"
+import numpy from "../data/images/numpy.jpg"
+import pandas from "../data/images/pandas.png"
+import seaborn from "../data/images/seaborn.jpg"
+import matplotlib from "../data/images/matplotlib.jpg"
+import powerbi from "../data/images/powerbi.jpg"
+import streamlit from "../data/images/streamlit.png"
+import excel from "../data/images/exel.jpg"
+import Psyliq from "../data/images/psyliq50.jpg"
+import Meriskill from "../data/images/meri50.jpg"
+
+export const Bio = {
+  name: "Susheel Verma",
+  roles: [
+    "Data Analyst",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/SusheelVerma31",
+  resume:
+    "https://drive.google.com/file/d/1txFKZD3LNIXMc329jD9c995AU8LItOXK/view?usp=drive_link",
+  linkedin: "Please add a link",
+  twitter: "Please add a link",
+  insta: "Please add a link",
+  facebook: "Please add a link",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Flutter",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Flask",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Flask_logo.svg/1280px-Flask_logo.svg.png",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Data Analysis",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Numpy",
+        image:
+          numpy,
+      },      
+      {
+        name: "Pandas",
+        image:
+          pandas,
+      },
+      {
+        name: "Seaborn",
+        image:
+          seaborn,
+      },
+      {
+        name: "Matplotlib",
+        image:
+          matplotlib,
+      },
+      {
+        name: "Power BI",
+        image:
+          powerbi,
+      },
+      {
+        name: "Excel",
+        image:
+          excel,
+      },
+
+    ],
+  },
+  {
+    title: "Machine Learning",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Tenserflow",
+        image:
+          "https://static-00.iconduck.com/assets.00/tensorflow-icon-1911x2048-1m2s54vn.png",
+      },
+      {
+        name: "Keras",
+        image:
+          "https://miro.medium.com/v2/resize:fit:600/1*DKu_54iqz6C-p6ndo7rO3g.png",
+      },
+      {
+        name: "Jupyter",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Jupyter_logo.svg/1767px-Jupyter_logo.svg.png",
+      },
+      {
+        name: "Google Colab",
+        image:
+          // "https://assets.stickpng.com/images/63c2e4c9c4baad4ce22bf9ef.png",
+          google,
+      },
+      {
+        name: "Sk Learn Kit",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Scikit_learn_logo_small.svg/2560px-Scikit_learn_logo_small.svg.png",
+      },
+      {
+        name: "NLP",
+        image:
+          nlp,
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Streamlit",
+        image:
+          streamlit,
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "AWS",
+        image:
+          "https://download.logo.wine/logo/Amazon_Web_Services/Amazon_Web_Services-Logo.wine.png",
+      },
+      {
+        name: "Google Cloud",
+        image:
+          "https://static-00.iconduck.com/assets.00/google-cloud-platform-logo-icon-2048x1824-pg4wzspq.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: Psyliq,
+    role: "Data analyst intern",
+    company: "Psyliq",
+    date: "nov 2023-dec 2023",
+    desc: " During this internship, i got the opportunity to gain invaluable hands-on experience in various aspects of data analysis interpretation. My responsibilities include tasks such as data collection, analysis, visualization, and deriving. During this virtual learning internship,  i got  exposed to real-world projects and challenges that  provides me  valuable industry experience.",
+    skills: [
+      "Data Cleaning",
+      "Data Preprocessing",
+      "Data Anlysis",
+      "Communication",
+      "Presentation",
+    ],
+  },
+  {
+    id: 1,
+    img: Meriskill,
+    role: "Data analyst virtual intern",
+    company: "Meriskill",
+    date: " feb 2024-march 2024",
+    desc: " During this internship, i got the opportunity to gain invaluable hands-on experience in various aspects of data analysis interpretation. My responsibilities include tasks such as data collection, analysis, visualization, and deriving. During this virtual learning internship,  i got  exposed to real-world projects and challenges that  provides me  valuable industry experience.",
+    skills: [
+      "Data Cleaning",
+      "Data Preprocessing",
+      "Data Anlysis",
+      "Communication",
+      "Presentation",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: pratap,
+    school: "Pratap Institute of Technology and Science, Sikar",
+    date: "Oct 2021 - Sep 2025",
+    grade: "8.41 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Pratap Institute of Technology and Science, Sikar. I have completed 4 semesters and have a CGPA of 8.71. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Google Developers Student Club (GDSC) at KIIT, where I am learning and working on exciting projects with a team of talented Data Scietist.",
+    degree: "Bachelor of Technology - B.Tech, Computer Science and Engineering",
+    Link : ""
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Age and Gender Detection",
+    date: "Oct 2023 - Nov 2023",
+    description:
+      "A Age and Gender Detection App that pridict the age and gender using the face. It is a Deep Learning based project the pridict the age and gender using the help of model that is created by tha large amount of data and the data having multiple image samples for every category",
+    image: gender,
+    tags: [
+      "Python",
+      "OpenCV",
+      "TensorFlow",
+      "Image Processing",
+      "Deep Learning",
+      "Computer Vision",
+      "Steamlit",
+    ],
+    category: "Deep Learning",
+    github: "https://github.com/SusheelVerma31/Age_and_gender_detection",
+    // webapp: "https://decisionhub.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Emotion Detection",
+    date: "Aug 2023 - Aug 2023",
+    description:
+      "A Emotion Detection App that pridict the Facial emotion using the face. It is a Deep Learning based project the pridict the facial emotion using the help of model that is created by tha large amount of data and the data having multiple image samples for every category",
+    image: Emotion,
+    tags: [
+      "Python",
+      "OpenCV",
+      "TensorFlow",
+      "Image Processing",
+      "Deep Learning",
+      "Computer Vision",
+      "Steamlit",
+    ],
+    category: "Deep Learning",
+    github: "https://github.com/SusheelVerma31/EMOTION_DETECTION",
+    // webapp: "https://decisionhub.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Smart Attendence System",
+    date: "july 2023 - Aug 2023",
+    description:
+      "A Smart Attendence App that is take your attendence using the your face. It is a Deep Learning based project that is take attendence using the face and using the help of your image that is created by tha  your face samples data and the data.",
+    image: SVM,
+    tags: [
+      "Python",
+      "OpenCV",
+      "TensorFlow",
+      "Image Processing",
+      "Deep Learning",
+      "Computer Vision",
+      "Steamlit",
+    ],
+    category: "Deep Learning",
+    github: "https://github.com/SusheelVerma31/SMART_ATTENDENCE_SYSTEM",
+    // webapp: "https://decisionhub.netlify.app/",
+  },
+  {
+    id: 4,
+    title: "Parking Space",
+    date: "sep 2023 - oct 2023",
+    description:
+      "A Paking Detection App that pridict the parking space in the parking area. It is a Deep Learning based project the detect the parking space using the help of model that is created by tha large amount of data and the data having multiple image samples for every category",
+    image: Parking_Space,
+    tags: [
+      "Python",
+      "OpenCV",
+      "Image Processing",
+      "Deep Learning",
+      "TensorFlow",
+      "Computer Vision",
+    ],
+    category: "Deep Learning",
+    github: "https://github.com/SusheelVerma31/parking-space-counter",
+    // webapp: "https://decisionhub.netlify.app/",
+  },
+  {
+    id: 5,
+    title: "Number Plate Detection",
+    date: "oct 2023 - oct 2023",
+    description:
+      "A Number Plate Detection App that detect the all vehicles and their no. plate that are running on the road . It is a Deep Learning based project the detect the vehicles and no. plates using the help of YOLOV8 model that is created by tha large amount of data and the data having multiple image samples for every category",
+    image: Number_Plate_Detection,
+    tags: [
+      "Python",
+      "OpenCV",
+      "TensorFlow",
+      "Image Processing",
+      "Deep Learning",
+      "Computer Vision",
+    ],
+    category: "Deep Learning",
+    github: "https://github.com/SusheelVerma31/automatic-number-plate-recognition",
+    // webapp: "https://decisionhub.netlify.app/",
+  },
+  {
+    id: 6,
+    title: "Color Detection",
+    date: "Nov 2023 - Nov 2023",
+    description:
+      "A Color Detection App that detect the color in the input. It is a Deep Learning based project detect the Color using the help of model that is created by tha large amount of data and the data having multiple image samples for every category",
+    image: Color_Detection,
+    tags: [
+      "Python",
+      "OpenCV",
+      "Image Processing",
+      "Deep Learning",
+      "TensorFlow",
+      "Computer Vision",
+    ],
+    category: "Deep Learning",
+    github: "https://github.com/SusheelVerma31/Color_Detection",
+    // webapp: "https://decisionhub.netlify.app/",
+  },
+  {
+    id: 7,
+    title: "Movie Recommended System",
+    date: "June 2022 - July 2022",
+    description:
+      "A Movie Recommended System that is recommend the next five movies releted to input. It is a Machine Learning based project recommend the five movies using the help of model that is created by the dataset.",
+    image: Movie_Recommended_System,
+    tags: [
+      "Python",
+      "Numpy",
+      "Pandas",
+      "Pickle",
+      "Sklearn",
+      "Data Anlytics",
+      "Data Visualization",
+      "Machine Learning"
+    ],
+    category: "Machine Learning",
+    github: "https://github.com/SusheelVerma31/movie-recommender-system-tmdb-dataset-project",
+    // webapp: "https://decisionhub.netlify.app/",
+  },
+  {
+    id: 8,
+    title: "SMS Spam Detection",
+    date: "May 2022 - June 2022",
+    description:
+      "A SMS Spam Detection that is dectect the sms is spam or not. It is a Machine Learning based project detects the smap or not using the help of model that is created by the dataset.",
+    image: SMS_Spam_Detection,
+    tags: [
+      "Python",
+      "Numpy",
+      "Pandas",
+      "Pickle",
+      "Sklearn",
+      "Data Anlytics",
+      "Data Visualization",
+      "Machine Learning"
+    ],
+    category: "Machine Learning",
+    github: "https://github.com/SusheelVerma31/sms-spam-classifier-main",
+    // webapp: "https://decisionhub.netlify.app/",
+  },
+  {
+    id: 9,
+    title: "Olympic Data Analysis",
+    date: "Feb 2022 - March 2022",
+    description:
+      "A Olympic Data Analysis in the analyze a olympic dataset and create parttens. It is a Data Analysis Project",
+    image: Olympic_Data_Analysis,
+    tags: [
+      "Python",
+      "Numpy",
+      "Pandas",
+      "Pickle",
+      "Sklearn",
+      "Data Anlytics",
+      "Data Visualization",
+    ],
+    category: "Data Analysis",
+    github: "https://github.com/SusheelVerma31/olympics-data-analysis-main",
+    // webapp: "https://decisionhub.netlify.app/",
+  },
+  {
+    id: 10,
+    title: "WhatsApp Chat Analysis",
+    date: "Feb 2022 - March 2022",
+    description:
+      "A WhatsApp Chat Analysis in the analyze a whatsapp chat using the chat dataset and create parttens. It is a Data Analysis Project",
+    image: WhatsApp_Chat_Analysis,
+    tags: [
+      "Python",
+      "Numpy",
+      "Pandas",
+      "Pickle",
+      "Sklearn",
+      "Data Anlytics",
+      "Data Visualization",
+    ],
+    category: "Data Analysis",
+    github: "https://github.com/SusheelVerma31/olympics-data-analysis-main",
+    // webapp: "https://decisionhub.netlify.app/",
+  },
+  {
+    id: 11,
+    title: "Twitter Sentiment Analysis",
+    date: "Dec 2023 - Jan 2024",
+    description:
+      "A Twitter-Sentiment-Analysis that sentiment the twitter comments accourding to cotegory. It is a Natural Language Processing based project that sentiments the twitter tweets and comments using the help of model that is created by tha large amount of data and the data having multiple categories",
+    image: Twitter_Sentiment_Analysis,
+    tags: [
+      "Python",
+      "Numpy",
+      "Pandas",
+      "Pickle",
+      "Sklearn",
+      "Deep Learning",
+      "TensorFlow",
+      "NLP",
+      "Streamlit",
+    ],
+    category: "NLP",
+    github: "https://github.com/SusheelVerma31/NLP-Project-Twitter-Sentiment-Analysis",
+    // webapp: "https://decisionhub.netlify.app/",
+  },
+  {
+    id: 12,
+    title: "Resume Screening App",
+    date: "Feb 2024 - Feb 2024",
+    description:
+      "A Resume Screening App that see the all data in the resume and fix the resume. It is a Natural Language Processing based project that seen the resume and fix the all errors using the help of model that is created by tha dataset",
+    image: Resume_Screening_App,
+    tags: [
+      "Python",
+      "Numpy",
+      "Pandas",
+      "Pickle",
+      "Sklearn",
+      "Deep Learning",
+      "TensorFlow",
+      "NLP",
+      "Streamlit",
+    ],
+    category: "NLP",
+    github: "https://github.com/SusheelVerma31/NLP-Project-Twitter-Sentiment-Analysis",
+    // webapp: "https://decisionhub.netlify.app/",
+  },
+];
+
