cc736374ae934bfdaa6cc5081db664c20e4063f8
Susheel Verma <150130897+SusheelVerma31@users.noreply.github.com>
2024-05-27 08:15:35 +0530

    Update Hero.jsx

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Bio } from "../../data/constants";
 import Typewriter from "typewriter-effect";
-import HeroImg from "../../images/kamal.jpg";
+import HeroImg from "../../images/susheel.jpg";
 import HeroBgAnimation from "../HeroBgAnimation";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
